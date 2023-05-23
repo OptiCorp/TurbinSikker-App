ca0e903a14a98531a880e19219bafc51a45c131c
malasen <malasenkan.mu@gmail.com>
2023-05-23 15:10:30 +0200

    feat: Auth, first iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
 
-import { Login } from './Pages/Login'
-import './BasicStyling/App.css'
+import { Login } from './Pages/Login';
+import './BasicStyling/App.css';
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { LandingPage } from './Pages/LandingPage/LandingPage';
-
 import Layout from "./components/Layout";
+import { useMsalAuthentication, useIsAuthenticated, useMsal} from "@azure/msal-react";
 
 const App = () =>  {
+  const {login, result, error} = useMsalAuthentication("redirect");
+  const isAuthenticated = useIsAuthenticated();
+
+  useEffect(() => {
+    if (error) {
+        console.log(error)
+    }
+  });
+
+  const { accounts } = useMsal();
 
   return (
-      <div className='wrapper'>
-       
-        	<Routes>
-          <Route path="/" element={<Login />}/>
-          <Route element={<Layout />}>
-      
-     
-            <Route path= 'LandingPage' element={<LandingPage/>}/>
-            </Route>
-     </Routes>
-  
-     
-      </div>
-    
+    <div className='wrapper'>
+            {isAuthenticated && (
+              <Routes>
+              <Route path="/" element={<Login />}/>
+              <Route element={<Layout />} >
+                  <Route path= 'LandingPage' element={<LandingPage/>}/>
+              </Route>
+            </Routes>
+            )}
+            {!isAuthenticated && (
+                <p>No users are signed in!</p>
+            )}
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
-import '././BasicStyling/index.css'
+import { MsalProvider } from "@azure/msal-react";
+import { Configuration,  PublicClientApplication } from "@azure/msal-browser";
+
+const configuration: Configuration = {
+  auth: {
+      clientId: "73b505c0-1edd-4fcd-8545-d024892a11d5"
+  }
+};
+
+const pca = new PublicClientApplication(configuration);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <BrowserRouter>   <App />
+    <MsalProvider instance={pca}>
+      <BrowserRouter>   
+        <App />
       </BrowserRouter>
- 
+    </MsalProvider>
   </React.StrictMode>,
 )
