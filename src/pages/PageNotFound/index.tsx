33b192241e7226c7d5f04c524f133396f48c3088
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-08-21 12:56:02 +0200

    feat: adding page not found component

---
+++ src/pages/PageNotFound/index.tsx
@@ -0,0 +1,13 @@
+import { PageNotFoundWrapper } from "./styles";
+
+function PageNotFound() {
+  return (
+    <PageNotFoundWrapper>
+      <h1>Page not found</h1>
+      <p>Contact support</p>
+      <a href="/">Go back to the home page</a>
+    </PageNotFoundWrapper>
+  );
+}
+
+export default PageNotFound;
---
+++ src/pages/PageNotFound/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const PageNotFoundWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
