9092607d09abed8093b9af5473876f36f010265d
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-08-21 12:23:58 +0200

    refactor: changing endpoints

--- src/components/addtasks/useAddTaskForm/index.tsx
+++ src/components/addtasks/useAddTaskForm/index.tsx
@@ -1,84 +1,82 @@
-import { SnackbarContext } from '@components/snackbar/SnackBarContext'
-import { useContext, useEffect, useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { useLocation, useParams } from 'react-router'
-import { CheckListEntity } from 'src/models/CheckListEntity'
-import { FormValuesEntity } from 'src/models/FormValuesEntity'
-import { TaskEntity } from 'src/models/TaskEntity'
-import { useApiContext } from '../../../pages/context/apiContextProvider'
-import useAuth from '../../../pages/landingPage/context/LandingPageContextProvider'
+import { SnackbarContext } from "@components/snackbar/SnackBarContext";
+import { useContext, useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useLocation, useParams } from "react-router";
+import { CheckListEntity } from "src/models/CheckListEntity";
+import { FormValuesEntity } from "src/models/FormValuesEntity";
+import { TaskEntity } from "src/models/TaskEntity";
+import { useApiContext } from "../../../pages/context/apiContextProvider";
+import useAuth from "../../../pages/landingPage/context/LandingPageContextProvider";
 
 export const useAddTaskForm = () => {
-    const { id } = useParams()
-    const { idToken } = useAuth()
-    const appLocation = useLocation()
+  const { id } = useParams();
+  const { idToken } = useAuth();
+  const appLocation = useLocation();
 
-    const methods = useForm<FormValuesEntity>()
-    const { reset, watch, handleSubmit, register, control } = methods
-    const { openSnackbar } = useContext(SnackbarContext)
-    const { refreshList, setRefreshList } = useApiContext()
+  const methods = useForm<FormValuesEntity>();
+  const { reset, watch, handleSubmit, register, control } = methods;
+  const { openSnackbar } = useContext(SnackbarContext);
+  const { refreshList, setRefreshList } = useApiContext();
 
-    const [selectedOption] = useState('')
-    const [selectedTask] = useState('')
-    const [checkListId, setCheckListId] = useState<CheckListEntity | null>(null)
-    const [sortedTasks, setSortedTasks] = useState<TaskEntity[]>([])
+  const [selectedOption] = useState("");
+  const [selectedTask] = useState("");
+  const [checkListId, setCheckListId] = useState<CheckListEntity | null>(null);
+  const [sortedTasks, setSortedTasks] = useState<TaskEntity[]>([]);
 
-    const onSubmit: SubmitHandler<FormValuesEntity> = async (data) => {
-        const res = await fetch(
-            `http://20.251.37.226:8080/api/AddTaskToChecklist?checklistId=${id}&taskId=${data.task}`,
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${idToken}`,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data.task),
-            }
-        )
-        if (res.ok) setRefreshList((prev) => !prev)
+  const onSubmit: SubmitHandler<FormValuesEntity> = async (data) => {
+    const res = await fetch(
+      `https://localhost:7290/api/AddTaskToChecklist?checklistId=${id}&taskId=${data.task}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data.task),
+      }
+    );
+    if (res.ok) setRefreshList((prev) => !prev);
 
-        if (openSnackbar) {
-            openSnackbar('Task added!')
-        }
+    if (openSnackbar) {
+      openSnackbar("Task added!");
     }
+  };
 
-    const fetchAllCheckListsId = async () => {
-        const res = await fetch(
-            `http://20.251.37.226:8080/api/GetChecklist?id=${id}`
-        )
-        if (!res.ok) throw new Error('Failed with HTTP code ' + res.status)
-        const data = await res.json()
-        const sorted = data.tasks.sort((a: any, b: any) => {
-            if (a.category.name < b.category.name) {
-                return -1
-            } else if (a.category.name > b.category.name) {
-                return 1
-            } else {
-                return 0
-            }
-        })
+  const fetchAllCheckListsId = async () => {
+    const res = await fetch(`https://localhost:7290/api/GetChecklist?id=${id}`);
+    if (!res.ok) throw new Error("Failed with HTTP code " + res.status);
+    const data = await res.json();
+    const sorted = data.tasks.sort((a: any, b: any) => {
+      if (a.category.name < b.category.name) {
+        return -1;
+      } else if (a.category.name > b.category.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
 
-        setSortedTasks(sorted)
-        setCheckListId(data)
-    }
+    setSortedTasks(sorted);
+    setCheckListId(data);
+  };
 
-    useEffect(() => {
-        fetchAllCheckListsId()
-    }, [refreshList])
+  useEffect(() => {
+    fetchAllCheckListsId();
+  }, [refreshList]);
 
-    return {
-        methods,
-        onSubmit,
-        control,
-        register,
+  return {
+    methods,
+    onSubmit,
+    control,
+    register,
 
-        handleSubmit,
-        watch,
-        location: appLocation.pathname,
-        selectedOption,
-        reset,
-        selectedTask,
-        sortedTasks,
-        checkListId,
-    }
-}
+    handleSubmit,
+    watch,
+    location: appLocation.pathname,
+    selectedOption,
+    reset,
+    selectedTask,
+    sortedTasks,
+    checkListId,
+  };
+};
--- src/pages/checklist/editchecklist/hooks/useEditCheckList/index.tsx
+++ src/pages/checklist/editchecklist/hooks/useEditCheckList/index.tsx
@@ -1,140 +1,140 @@
-import { useAddTaskForm } from '@components/addtasks/useAddTaskForm'
-import { SnackbarContext } from '@components/snackbar/SnackBarContext'
-import { useContext, useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router'
-import { CheckListEntity } from 'src/models/CheckListEntity'
-import { TaskEntity } from 'src/models/TaskEntity'
-import { useApiContext } from '../../../../context/apiContextProvider'
-import useAuth from '../../../../landingPage/context/LandingPageContextProvider'
+import { useAddTaskForm } from "@components/addtasks/useAddTaskForm";
+import { SnackbarContext } from "@components/snackbar/SnackBarContext";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { CheckListEntity } from "src/models/CheckListEntity";
+import { TaskEntity } from "src/models/TaskEntity";
+import { useApiContext } from "../../../../context/apiContextProvider";
+import useAuth from "../../../../landingPage/context/LandingPageContextProvider";
 
 export const useEditCheckList = () => {
-    const { idToken } = useAuth()
-    const { checkListId } = useAddTaskForm()
-    const navigate = useNavigate()
-    const handleOpen = (
-        taskId: string,
-        taskDescription: string,
-        categoryId: string
-    ) => {
-        setTaskId(taskId)
-        setCategoryId(categoryId)
-        setTaskDescription(taskDescription)
+  const { idToken } = useAuth();
+  const { checkListId } = useAddTaskForm();
+  const navigate = useNavigate();
+  const handleOpen = (
+    taskId: string,
+    taskDescription: string,
+    categoryId: string
+  ) => {
+    setTaskId(taskId);
+    setCategoryId(categoryId);
+    setTaskDescription(taskDescription);
+  };
+
+  const [task, setTask] = useState<TaskEntity>();
+  const [taskId, setTaskId] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [title, setTitle] = useState<CheckListEntity | any>();
+  const [isOpenn, setIsOpenn] = useState(false);
+
+  const { id } = useParams();
+
+  const handleTitleChange = (title: string) => {
+    setTitle(title);
+  };
+
+  const { openSnackbar } = useContext(SnackbarContext);
+  const { setRefreshList } = useApiContext();
+
+  const [checked, setChecked] = useState(!!checkListId?.status);
+
+  const [isOpenNew, setIsOpenNew] = useState(false);
+
+  const handleCloseNewCheckList = () => {
+    setIsOpenNew(false);
+  };
+
+  useEffect(() => {
+    setChecked(checkListId?.status === "Active");
+  }, [checkListId]);
+
+  function convertStatusToString(status: boolean): "Active" | "Inactive" {
+    return status ? "Active" : "Inactive";
+  }
+
+  useEffect(() => {
+    if (checkListId && checkListId.tasks.length === 0) {
+      setIsOpenn(true);
     }
+  }, [checkListId]);
 
-    const [task, setTask] = useState<TaskEntity>()
-    const [taskId, setTaskId] = useState('')
-    const [categoryId, setCategoryId] = useState('')
-    const [taskDescription, setTaskDescription] = useState('')
-    const [title, setTitle] = useState<CheckListEntity | any>()
-    const [isOpenn, setIsOpenn] = useState(false)
+  const updateCheckListTask = async (data: {
+    taskId: string;
+    description: string;
+    categoryId: string;
+  }) => {
+    const res = await fetch(
+      `http://20.251.37.226:8080/api/UpdateChecklistTask?id=${data.taskId}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          description: data.description,
+          categoryId: data.categoryId,
+        }),
+      }
+    );
+    if (res.ok) setRefreshList((prev) => !prev);
 
-    const { id } = useParams()
-
-    const handleTitleChange = (title: string) => {
-        setTitle(title)
+    if (openSnackbar) {
+      openSnackbar(`Task updated`);
     }
+  };
 
-    const { openSnackbar } = useContext(SnackbarContext)
-    const { setRefreshList } = useApiContext()
-
-    const [checked, setChecked] = useState(!!checkListId?.status)
-
-    const [isOpenNew, setIsOpenNew] = useState(false)
-
-    const handleCloseNewCheckList = () => {
-        setIsOpenNew(false)
+  const handleSave = async (data: { title: string; status: string }) => {
+    const res = await fetch(
+      `https://localhost:7290/api/UpdateChecklist?id=${id}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: data.title || title,
+          status: data.status,
+        }),
+      }
+    );
+    if (res.ok) {
+      setRefreshList((prev) => !prev);
     }
+    navigate("/MyChecklists");
 
-    useEffect(() => {
-        setChecked(checkListId?.status === 'Active')
-    }, [checkListId])
-
-    function convertStatusToString(status: boolean): 'Active' | 'Inactive' {
-        return status ? 'Active' : 'Inactive'
+    if (openSnackbar) {
+      openSnackbar(`Checklist updated`);
+      setTimeout(() => openSnackbar(""), 3000);
     }
+  };
 
-    useEffect(() => {
-        if (checkListId && checkListId.tasks.length === 0) {
-            setIsOpenn(true)
-        }
-    }, [checkListId])
+  return {
+    setTaskId,
+    task,
+    setTask,
+    handleOpen,
 
-    const updateCheckListTask = async (data: {
-        taskId: string
-        description: string
-        categoryId: string
-    }) => {
-        const res = await fetch(
-            `http://20.251.37.226:8080/api/UpdateChecklistTask?id=${data.taskId}`,
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${idToken}`,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    description: data.description,
-                    categoryId: data.categoryId,
-                }),
-            }
-        )
-        if (res.ok) setRefreshList((prev) => !prev)
+    categoryId,
+    taskDescription,
+    taskId,
 
-        if (openSnackbar) {
-            openSnackbar(`Task updated`)
-        }
-    }
+    updateCheckListTask,
+    handleSave,
+    title,
+    setTitle,
 
-    const handleSave = async (data: { title: string; status: string }) => {
-        const res = await fetch(
-            `http://20.251.37.226:8080/api/UpdateChecklist?id=${id}`,
-            {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${idToken}`,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    title: data.title || title,
-                    status: data.status,
-                }),
-            }
-        )
-        if (res.ok) {
-            setRefreshList((prev) => !prev)
-        }
-        navigate('/MyChecklists')
+    handleTitleChange,
+    checked,
+    setChecked,
+    convertStatusToString,
+    isOpenn,
+    setIsOpenn,
 
-        if (openSnackbar) {
-            openSnackbar(`Checklist updated`)
-            setTimeout(() => openSnackbar(''), 3000)
-        }
-    }
-
-    return {
-        setTaskId,
-        task,
-        setTask,
-        handleOpen,
-
-        categoryId,
-        taskDescription,
-        taskId,
-
-        updateCheckListTask,
-        handleSave,
-        title,
-        setTitle,
-
-        handleTitleChange,
-        checked,
-        setChecked,
-        convertStatusToString,
-        isOpenn,
-        setIsOpenn,
-
-        isOpenNew,
-        setIsOpenNew,
-        handleCloseNewCheckList,
-    }
-}
+    isOpenNew,
+    setIsOpenNew,
+    handleCloseNewCheckList,
+  };
+};
