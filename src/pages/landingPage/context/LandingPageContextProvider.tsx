f47d64f8bbbac28bc0ea97721581c6993946be10
Eirik Bakkan <eirik.bakkan@bouvet.no>
2023-08-24 13:16:28 +0200

    added scope

--- src/pages/landingPage/context/LandingPageContextProvider.tsx
+++ src/pages/landingPage/context/LandingPageContextProvider.tsx
@@ -56,7 +56,6 @@ export function AuthProvider({
           "user.read",
           "email",
           "api://c1b6fd67-db9e-4a25-a4ad-9aec1fe3ec64/user.read",
-          "api://b1bc9eb7-71fc-43c4-a888-ae73c72be014/ReadWrite"
         ],
         account: accounts[0],
       };
