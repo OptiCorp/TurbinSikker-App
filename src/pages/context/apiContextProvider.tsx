622d847c398ddeea3dcaae3203bd1542d6feb1e8
Eirik Bakkan <eirikbakkan@gmail.com>
2023-08-31 14:13:11 +0200

    removed authorization

--- src/pages/context/apiContextProvider.tsx
+++ src/pages/context/apiContextProvider.tsx
@@ -156,13 +156,13 @@ const ApiContextProvider = ({ children }: { children: React.ReactNode }) => {
   const getUsers = async () => {
 
     const res = await fetch("https://localhost:7290/api/GetAllUsersAdmin",
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${idToken}`,
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": '*'
-      }}
+    // {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: `Bearer ${idToken}`,
+    //     "Content-Type": "application/json",
+    //     "Access-Control-Allow-Origin": '*'
+    //   }}
     );
     if (!res.ok) throw new Error("Failed with HTTP code " + res.status);
     const data = await res.json();
