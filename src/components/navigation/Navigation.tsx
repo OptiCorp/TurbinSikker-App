92cfe4f56847291d8795804e2f159b7d5be91398
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-09-08 09:40:27 +0200

    feat: tab 1 navigates to list of punches

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,90 +1,88 @@
-import { Icon, Tabs } from "@equinor/eds-core-react";
-import { assignment, menu } from "@equinor/eds-icons";
-import { useState } from "react";
-import { useLocation, useNavigate } from "react-router";
-import Sidebar from "../sidebar/Sidebar";
-import {
-  FooterContainer,
-  ImageContainer,
-  ImageContainerActive,
-} from "./styles";
+import { Icon, Tabs } from '@equinor/eds-core-react'
+import { assignment, menu } from '@equinor/eds-icons'
+import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
+import Sidebar from '../sidebar/Sidebar'
+import { FooterContainer, ImageContainer, ImageContainerActive } from './styles'
 
 export const Navigation: React.FC = () => {
-  const [activeTab, setActiveTab] = useState(1);
-  const navigate = useNavigate();
-  const handleChange = (index: number) => {
-    setActiveTab(index);
-  };
-  const [open, setOpen] = useState(false);
-  const appLocation = useLocation();
+    const [activeTab, setActiveTab] = useState(1)
+    const navigate = useNavigate()
+    const handleChange = (index: number) => {
+        setActiveTab(index)
+    }
+    const [open, setOpen] = useState(false)
+    const appLocation = useLocation()
 
-  const items = [
-    {
-      name: "Tab 1",
-      value: (index?: number) =>
-        activeTab === index ? (
-          <ImageContainerActive onClick={() => navigate("/AddPunch")} />
-        ) : (
-          <ImageContainer onClick={() => navigate("/AddPunch")} />
-        ),
-    },
-    {
-      name: "Tab 2",
-      value: (index?: number) => (
-        <Icon
-          data={assignment}
-          size={40}
-          style={{
-            color: activeTab === index ? "#73B1B5" : "white",
-            width: "min(500px)",
-          }}
-          onClick={() => navigate("/CheckList")}
-        />
-      ),
-    },
-    {
-      name: "Tab 3",
-      value: (index?: number) => (
-        <Icon
-          data={menu}
-          size={40}
-          style={{
-            color: activeTab === index ? "#73B1B5" : "white",
-            width: "min(500px)",
-          }}
-          onClick={() => setOpen(!open)}
-        />
-      ),
-    },
-  ];
+    const items = [
+        {
+            name: 'Tab 1',
+            value: (index?: number) =>
+                activeTab === index ? (
+                    <ImageContainerActive
+                        onClick={() => navigate('/ListPunches')}
+                    />
+                ) : (
+                    <ImageContainer onClick={() => navigate('/ListPunches')} />
+                ),
+        },
+        {
+            name: 'Tab 2',
+            value: (index?: number) => (
+                <Icon
+                    data={assignment}
+                    size={40}
+                    style={{
+                        color: activeTab === index ? '#73B1B5' : 'white',
+                        width: 'min(500px)',
+                    }}
+                    onClick={() => navigate('/CheckList')}
+                />
+            ),
+        },
+        {
+            name: 'Tab 3',
+            value: (index?: number) => (
+                <Icon
+                    data={menu}
+                    size={40}
+                    style={{
+                        color: activeTab === index ? '#73B1B5' : 'white',
+                        width: 'min(500px)',
+                    }}
+                    onClick={() => setOpen(!open)}
+                />
+            ),
+        },
+    ]
 
-  return (
-    <>
-      <Sidebar open={open} setOpen={setOpen} />
-      {appLocation.pathname === "/AddUser/" ||
-      appLocation.pathname.includes("EditUser") ||
-      appLocation.pathname.includes("MyCheckLists") ||
-      appLocation.pathname.includes("EditCheckList") ||
-      appLocation.pathname.includes("/PreviewCheckList/") ||
-      appLocation.pathname.includes("/FillOutCheckList/") ||
-      appLocation.pathname.includes("AddPunch") ||
-      appLocation.pathname.includes("SendCheckList") ? null : (
-        <FooterContainer>
-          <Tabs activeTab={activeTab} onChange={handleChange}>
-            <Tabs.List
-              style={{
-                display: "grid",
-                gridTemplateColumns: "1fr 1fr 1fr",
-                gap: "70px",
-              }}
-            >
-              {items.map(({ name, value }, index) => (
-                <Tabs.Tab key={name}>{value(index)}</Tabs.Tab>
-              ))}
-            </Tabs.List>
-          </Tabs>
-        </FooterContainer>
-      )}
-    </>
-  );
-};
+    return (
+        <>
+            <Sidebar open={open} setOpen={setOpen} />
+            {appLocation.pathname === '/AddUser/' ||
+            appLocation.pathname.includes('EditUser') ||
+            appLocation.pathname.includes('MyCheckLists') ||
+            appLocation.pathname.includes('EditCheckList') ||
+            appLocation.pathname.includes('/PreviewCheckList/') ||
+            appLocation.pathname.includes('/FillOutCheckList/') ||
+            appLocation.pathname.includes('AddPunch') ||
+            appLocation.pathname.includes('SendCheckList') ? null : (
+                <FooterContainer>
+                    <Tabs activeTab={activeTab} onChange={handleChange}>
+                        <Tabs.List
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: '1fr 1fr 1fr',
+                                gap: '70px',
+                            }}
+                        >
+                            {items.map(({ name, value }, index) => (
+                                <Tabs.Tab key={name}>{value(index)}</Tabs.Tab>
+                            ))}
+                        </Tabs.List>
+                    </Tabs>
+                </FooterContainer>
+            )}
+        </>
+    )
+}
