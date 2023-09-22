dc87eb1eb6cc588406464da4eb3b7a2e86f7fb9f
Kathrine Stangeland <kathrine.kodehode@gmail.com>
2023-09-22 11:48:50 +0200

    WIP dont merge yet

--- src/pages/checklist/fillOutChecklist/index.tsx
+++ src/pages/checklist/fillOutChecklist/index.tsx
@@ -1,9 +1,4 @@
-import CustomDialog from '@components/modal/useModalHook'
-import { Typography } from '@equinor/eds-core-react'
-import { useState } from 'react'
 import { FormProvider } from 'react-hook-form'
-import { checkList } from '../../../pages/context/models/checklist'
-import { EditListPoints } from '../editchecklist/styles'
 import { Wrapper } from '../previewCheckList/styles'
 import { useWorkflowContext } from '../workflow/context/workFlowContextProvider'
 import { useFillOutCheckList } from './FillOutCheckListHook'
@@ -11,15 +6,11 @@ import { FillOutList } from './FillOutList'
 import { AddPunchHeader, StyledCard, StyledCardHeader } from './styles'
 
 export const FillOutCheckList = () => {
-    const { workFlowById } = useWorkflowContext()
-
-    const [content, setContent] = useState('')
-    const [commentDialogShowing, setCommentDialogShowing] = useState(false)
-    const [punchDialogShowing, setPunchDialogShowing] = useState(false)
+    const { workFlowById, WorkFlows } = useWorkflowContext()
 
     const { methods, onUpdate } = useFillOutCheckList()
     const { handleSubmit } = methods
-
+    console.log(workFlowById?.checklist?.checklistTasks)
     return (
         <>
             <FormProvider {...methods}>
@@ -28,38 +19,7 @@ export const FillOutCheckList = () => {
                         <div>
                             <AddPunchHeader>
                                 <StyledCard>
-                                    <StyledCardHeader>
-                                        <Typography
-                                            onClick={() =>
-                                                setCommentDialogShowing(true)
-                                            }
-                                            token={{
-                                                textAlign: 'center',
-                                                fontWeight: 600,
-                                                fontSize: '0.8rem',
-                                                color: 'green',
-                                            }}
-                                            link
-                                            href="#"
-                                        >
-                                            Add comment
-                                        </Typography>
-                                        <Typography
-                                            onClick={() =>
-                                                setPunchDialogShowing(true)
-                                            }
-                                            token={{
-                                                textAlign: 'center',
-                                                fontWeight: 600,
-                                                fontSize: '0.8rem',
-                                                color: 'red',
-                                            }}
-                                            link
-                                            href="#"
-                                        >
-                                            Add punch
-                                        </Typography>
-                                    </StyledCardHeader>
+                                    <StyledCardHeader></StyledCardHeader>
                                 </StyledCard>
                             </AddPunchHeader>
                         </div>
@@ -78,55 +38,6 @@ export const FillOutCheckList = () => {
                                 )
                             )}
                         </Wrapper>
-                        <CustomDialog
-                            isOpen={commentDialogShowing}
-                            negativeButtonOnClick={() =>
-                                setCommentDialogShowing(false)
-                            }
-                            title="Add comment?"
-                            negativeButtonText="Cancel"
-                            positiveButtonText="Save"
-                            buttonVariant="ghost"
-                        >
-                            <Typography
-                                variant="text"
-                                token={{ textAlign: 'left' }}
-                            >
-                                Comment will be added to the end of the form.
-                            </Typography>
-                            <EditListPoints
-                                label=""
-                                key={checkList.id ?? ''}
-                                id="storybook-multi-readonly"
-                                multiline
-                                rows={5}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) => {
-                                    setContent(event.target.value)
-                                }}
-                            />
-                        </CustomDialog>
-
-                        <CustomDialog
-                            title="Make Punch?"
-                            buttonVariant="ghost"
-                            negativeButtonOnClick={() =>
-                                setPunchDialogShowing(false)
-                            }
-                            negativeButtonText="Cancel"
-                            positiveButtonText="OK"
-                            isOpen={punchDialogShowing}
-                        >
-                            <Typography
-                                group="input"
-                                variant="text"
-                                token={{ textAlign: 'left' }}
-                            >
-                                You will be forwarded to Punch form. You will be
-                                able to continue this form where you left after.
-                            </Typography>
-                        </CustomDialog>
                     </div>
                 </form>
             </FormProvider>
--- src/pages/punch/addPunch/AddPunchHook.tsx
+++ src/pages/punch/addPunch/AddPunchHook.tsx
@@ -1,11 +1,13 @@
 import { SnackbarContext } from '@components/snackbar/SnackBarContext'
-import { SetStateAction, useContext, useEffect, useState } from 'react'
+import { SetStateAction, useContext, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useLocation, useNavigate, useParams } from 'react-router'
 import { API_URL } from '../../../config'
+import { useWorkflowContext } from '../../../pages/checklist/workflow/context/workFlowContextProvider'
 import { useCheckListContext } from '../../../pages/context/CheckListContextProvider'
 import useAuth from '../../../pages/landingPage/context/LandingPageContextProvider'
 import { useUserContext } from '../../../pages/users/context/userContextProvider'
+import { usePunchContext } from '../context/PunchContextProvider'
 
 export const useAddPunch = () => {
     const { currentUser } = useUserContext()
@@ -18,22 +20,30 @@ export const useAddPunch = () => {
     const { accessToken } = useAuth()
     const navigate = useNavigate()
     const [positiveOpen, setPositiveOpen] = useState(false)
+    const { punch, taskId, workFlow, workflowData } = usePunchContext()
+    const { workFlowById } = useWorkflowContext()
 
+    const [test, setTest] = useState('')
     type FormValuesPunchEntity = {
+        id: string
         createdBy: string
-        punchDescription: string
+        taskId: string
+        description: string
         severity: string
         checklistTaskId: string
-        checklistWorkFlowId: string
+        workflowId: string
     }
 
     const [description, setDescription] = useState('')
     const [severity, setSeverity] = useState<SetStateAction<string>>('Minor')
 
-    const onSubmit: SubmitHandler<FormValuesPunchEntity> = async (data) => {
+    const onSubmit: SubmitHandler<FormValuesPunchEntity> = async (data: {
+        taskId: string
+        workflowId: string
+    }) => {
         if (appLocation.pathname === '/addPunch') {
             setDescription('')
-            await postPunch()
+            // await postPunch()
         } else {
             const res = await fetch(`${API_URL}/UpdatePunch?id=${id}`, {
                 method: 'POST',
@@ -42,8 +52,15 @@ export const useAddPunch = () => {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
                 },
-                body: JSON.stringify(data),
+                body: JSON.stringify({
+                    id: id,
+                    description: description,
+                    workflowId: data.workflowId,
+                    severity: severity,
+                    status: 'Pending',
+                }),
             })
+            console.log(workflowData)
             if (res.ok) setRefreshList((prev) => !prev)
             setPositiveOpen(false)
             navigate('/Checklist')
@@ -51,8 +68,9 @@ export const useAddPunch = () => {
         }
     }
 
-    async function postPunch() {
+    const postPunch = async (data: { taskId: string; workFlowId: string }) => {
         if (!accessToken) return
+
         const res = await fetch(`${API_URL}/AddPunch`, {
             method: 'POST',
             headers: {
@@ -61,19 +79,21 @@ export const useAddPunch = () => {
                 'Access-Control-Allow-Origin': '*',
             },
             body: JSON.stringify({
-                createdBy: currentUser?.id,
-                punchDescription: description,
+                creatorId: currentUser?.id,
+                description: description,
                 severity: severity,
-                checklistTaskId: '482f2505-c02d-47a0-8c00-e9b757662d0d',
-                checklistWorkFlowId: '4c782512-b9ed-4e4c-b5f2-8f0411a30340',
+                checkListTaskId: data.taskId,
+                workflowId: data.workFlowId,
             }),
         })
+
         if (res.ok) {
             const responseJson = await res.json()
             if (responseJson && responseJson.id) {
                 const punchId = responseJson.id
                 navigate(`/Punch/${punchId}`)
             }
+
             if (openSnackbar) {
                 openSnackbar(`Punch created`)
             }
@@ -99,5 +119,7 @@ export const useAddPunch = () => {
         setSeverity,
         handleOpen,
         clearAndClose,
+
+        postPunch,
     }
 }
