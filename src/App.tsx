a73d24dd265289e96e66616eec4a62bdddc4c9a5
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-10-11 13:24:49 +0200

    refactor: removing UserContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import { useIsAuthenticated, useMsal } from '@azure/msal-react'
 import { RoutesContainer } from './RoutesContainer'
 import './assets/App.css'
-
 import { GlobalProvider } from './context/globalContextProvider'
-
 import { Login } from './pages/login'
-import { UserContextProvider } from './pages/users/context/userContextProvider'
 
 const App = () => {
     const isAuthenticated = useIsAuthenticated()
-    const { inProgress } = useMsal()
 
     return (
         <div className="wrapper">
             {isAuthenticated && (
                 <GlobalProvider>
-                    <UserContextProvider>
-                        <RoutesContainer />
-                        {/* <SnackbarComponent />  */}
-                    </UserContextProvider>
+                    <RoutesContainer />
+                    {/* <SnackbarComponent />  */}
                 </GlobalProvider>
             )}
             {!isAuthenticated && <Login />}
