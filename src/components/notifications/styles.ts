a98e130aa89db2d004e1501cf46f18d5c1dd40dc
Henrik <henrik.laland@bouvet.no>
2023-11-09 11:32:45 +0100

    notification toable is now on top when empty

---
+++ src/components/notifications/styles.ts
@@ -0,0 +1,8 @@
+import { Dialog } from "@equinor/eds-core-react";
+import styled from "styled-components";
+
+export const StyledDialog = styled(Dialog)`
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+`;
